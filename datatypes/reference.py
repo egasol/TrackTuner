ea861ec1578ef82ea7e8e929fd4deadb7d1cd80a
egasol <simon.nasfeldt@gmail.com>
2025-03-04 19:40:49 +0100

    Add track generator class

--- datatypes/reference.py
+++ datatypes/reference.py
@@ -1,3 +1,5 @@
+import random
+import math
 from typing import Dict, List
 
 
@@ -13,5 +15,45 @@ class Reference:
 
 
 class ReferenceTrack:
-    def __init__(self):
-        self.track: List[Reference] = []
+    def __init__(
+        self,
+        id: int,
+        x: float,
+        y: float,
+        z: float,
+        start_frame: int,
+        end_frame: int,
+        mod_min: float = 0.10,
+        mod_max: float = 0.80,
+    ):
+        self.id = id
+        self.start_x = x
+        self.start_y = y
+        self.start_z = z
+        self.start_frame = start_frame
+        self.end_frame = end_frame
+
+        self.func_x = random.choice([math.sin, math.cos, lambda x: x])
+        self.func_y = random.choice([math.sin, math.cos, lambda x: x])
+        self.func_z = random.choice([math.sin, math.cos, lambda x: 10 * x])
+
+        self.mod_x = random.uniform(mod_min, mod_max)
+        self.mod_y = random.uniform(mod_min, mod_max)
+        self.mod_z = random.uniform(mod_min, mod_max)
+
+        self.track = []
+
+    def add(self, frame: int, x: float, y: float, z: float):
+        self.track.append(Reference(self.id, x, y, z))
+
+    def generate(self) -> Dict[int, Reference]:
+        positions = {}
+        x, y, z = self.start_x, self.start_y, self.start_z
+
+        for frame in range(self.start_frame, self.end_frame + 1):
+            positions[frame] = Reference(self.id, x, y, z)
+            x += self.func_x(self.mod_x * (frame - self.start_frame))
+            y += self.func_y(self.mod_y * (frame - self.start_frame))
+            z += self.func_z(self.mod_z * (frame - self.start_frame))
+
+        return positions
